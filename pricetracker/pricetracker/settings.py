dd656ebacc2ddf3b4ed7a4e7dfc6e1265b345247
yash <yashpatel7025@gmail.com>
2019-06-02 19:34:51 +0530

    debug change

--- pricetracker/pricetracker/settings.py
+++ pricetracker/pricetracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Falses
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
