855b2445d4a441c785296f78b6c01cf309d45fc6
yash <yashpatel7025@gmail.com>
2019-06-02 18:29:54 +0530

    Added Static file in settings

--- pricetracker/pricetracker/settings.py
+++ pricetracker/pricetracker/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')#for heroku deployment
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
