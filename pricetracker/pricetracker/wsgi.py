b8b214e942932b3152a081febacfa029c5b3a7b1
yash <yashpatel7025@gmail.com>
2019-06-03 00:23:25 +0530

    tp

--- pricetracker/pricetracker/wsgi.py
+++ pricetracker/pricetracker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricetracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricetrack.settings')
 
 application = get_wsgi_application()
