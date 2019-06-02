6eeab04554e356a63b88b291f9aecf489e3c0466
yash <yashpatel7025@gmail.com>
2019-06-02 22:46:08 +0530

    added python-dotenv

--- pricetracker/pricetracker/settings.py
+++ pricetracker/pricetracker/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '7c1b0c077bef519710b48c6fdac6bb7b501e6f4a3bb8540b'#os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
