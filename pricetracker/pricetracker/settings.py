5e24bce222f099a5c2a1a582f4860c7c80836d27
yash <yashpatel7025@gmail.com>
2019-06-02 19:41:01 +0530

    setting change

--- pricetracker/pricetracker/settings.py
+++ pricetracker/pricetracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '9rnmb%oze35^wi^9p3fy2p-z2=j=-x)+j869@4r9!n8nfia=f%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
