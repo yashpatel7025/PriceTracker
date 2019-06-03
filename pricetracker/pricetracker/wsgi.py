6bfcee879d2e66ff02ca95ce8acff1d5267206dd
yash <yashpatel7025@gmail.com>
2019-06-03 20:34:10 +0530

    debug True

--- pricetracker/pricetracker/wsgi.py
+++ pricetracker/pricetracker/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricetracker.settings')
--- pricetracker/wsgi.py
+++ pricetracker/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append("C:/Users/Aakash/Desktop/trackass/pricetracker")
 
 from django.core.wsgi import get_wsgi_application
 
